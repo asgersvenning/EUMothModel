f3566112f9bdba13b2270d66c26bf6a8781f71cd
asgersvenning <asgersvenning@gmail.com>
2023-10-12 15:35:43 +0200

    Removed custom css, using bootstrap instead due to CORB.

--- filtering/activeLearningForMothCropping.py
+++ filtering/activeLearningForMothCropping.py
@@ -285,19 +285,28 @@ def non_max_suppression(
     return output
 
 # Initialize your app
-style_sheet = [
-    # This would work on a local machine, but I need it to work through an SSH tunnel
-    # os.path.join(os.path.dirname(__file__), 'activeLearning.css')
-    # This works through an SSH tunnel
+styles = [
+    #<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     {
-        'href': 'https://raw.githubusercontent.com/asgersvenning/EUMothModel/main/filtering/activeLearning.css',
+        'href': 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
         'rel': 'stylesheet',
-        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'intregrity': 'sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN',
         'crossorigin': 'anonymous'
     }
 ]
 
-app = dash.Dash(__name__, external_stylesheets=style_sheet)
+scripts = [
+    #<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    {
+    #<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        'src': 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+        'integrity': 'sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL',
+        'crossorigin': 'anonymous'
+    }
+]
+
+
+app = dash.Dash(__name__, external_stylesheets=styles, external_scripts=scripts)
 app.config.suppress_callback_exceptions = True
 
 # Define the app state
@@ -412,8 +421,8 @@ app.layout = html.Div([
     html.Div(
         id='Controls', 
         children=[
-            html.Button('Next', id='next-button', className='control-button'),
-            html.Button('Test', id='test-button', className='control-button')
+            html.Button('Next', id='next-button', className='btn btn-primary', style={'margin': '10px'}),
+            html.Button('Test', id='test-button', className='btn btn-primary', style={'margin': '10px'})
         ],
         style={
             'display': 'flex', 
